19b7e781ca6b12497ed2c8a80a0efd80f3a67fde
Aliaksei Panasik <aliaksei_panasik@epam.com>
2020-06-11 11:09:47 +0300

    PHP parser was added

---
+++ bin/parser.php
@@ -0,0 +1,12 @@
+<?php
+
+$file = $argv[1] ?? null;
+if (!$file) {
+    echo "Please, specify a log file!\n";
+    exit(1);
+}
+
+echo "list of webpages with most page views ordered from most pages views to less page views\n";
+system('php ' . __DIR__ . '/log-reader -o "{{ path }} {{ path_cnt }} visits" --sort-key=path_cnt --sort-order=desc --group-by=path --group-unique=ip --schema="path ip" ' . $file);
+echo "\nlist of webpages with most unique page views also ordered\n";
+system('php ' . __DIR__ . '/log-reader -o "{{ path }} {{ ip_unique }} unique views" --sort-key=ip_unique --sort-order=desc --group-by=path --group-unique=ip --schema="path ip" ' . $file);
